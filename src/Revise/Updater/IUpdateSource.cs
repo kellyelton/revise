8ab0d5b62a0e92f7b6e1bc318ba1da7e7a8b149f
Kelly Elton <kelly.elton@skylabsonline.com>
2013-08-27 13:12:41 -0500

    Added some updating stuff.

---
+++ src/Revise/Updater/IUpdateSource.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Revise.Updater
+{
+    public interface IUpdateSource
+    {
+        /// <summary>
+        /// Name of this <see cref="IUpdateSource"/>. Used mostly for logging/debugging.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Current version we are at
+        /// </summary>
+        Version CurrentVersion { get; }
+
+        event EventHandler<ProgressEventArgs> OnDownloadUpdateProgressChange;
+
+        /// <summary>
+        /// Checks to see if there is an update available
+        /// </summary>
+        /// <returns>True if there is, False if there isn't</returns>
+        Task<bool> IsUpdateAvailable();
+
+        /// <summary>
+        /// Checks to see if there is a specific package with a specific version exists
+        /// </summary>
+        /// <param name="version">Version to see if exists</param>
+        /// <returns>True if there is one, False otherwise</returns>
+        Task<bool> UpdateVersionExists(Version version);
+
+        /// <summary>
+        /// Download an update 
+        /// </summary>
+        /// <param name="downloadLocation">Folder to download the package to</param>
+        /// <param name="specificVersion">Specific version to download if specified, null for latest</param>
+        /// <returns><see cref="IUpdatePackage"/>On success, otherwise null</returns>
+        Task<IUpdatePackage> DownloadUpdate(DirectoryInfo downloadLocation, Version specificVersion = null);
+    }
+
+    public interface IUpdatePackage
+    {
+        event EventHandler<ProgressEventArgs> OnExtractProgressChange;
+
+        /// <summary>
+        /// File path of the <see cref="IUpdatePackage"/>
+        /// </summary>
+        string FilePath { get; }
+
+        /// <summary>
+        /// <see cref="Version"/> of the <see cref="IUpdatePackage"/>
+        /// </summary>
+        Version Version { get; }
+
+        /// <summary>
+        /// Extract the package into a directory
+        /// </summary>
+        /// <param name="extractDirectory">Directory to extract to</param>
+        /// <returns>True if it succeeds, otherwise False</returns>
+        Task<bool> Extract(DirectoryInfo extractDirectory);
+    }
+
+    public class ProgressEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Progress out of 100%
+        /// </summary>
+        public int Progress { get; internal set; }
+
+        /// <summary>
+        /// Current message for the progress. Set to null if we don't want to use the last used message.
+        /// </summary>
+        public string Message { get; internal set; }
+
+        public ProgressEventArgs(int progress):this(progress,null)
+        {
+            
+        }
+
+        public ProgressEventArgs(int progress, string message, params object[] args)
+        {
+            Progress = progress;
+            Message = message == null ? null : string.Format(message, args);
+        }
+    }
+}
---
+++ src/Revise/Updater/UpdateManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Core;
+
+namespace Revise.Updater
+{
+    public class UpdateManager
+    {
+        internal static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        #region Singleton
+
+        internal static UpdateManager SingletonContext { get; set; }
+
+        private static readonly object UpdateManagerSingletonLocker = new object();
+
+        public static UpdateManager Instance
+        {
+            get
+            {
+                if (SingletonContext == null)
+                {
+                    lock (UpdateManagerSingletonLocker)
+                    {
+                        if (SingletonContext == null)
+                        {
+                            SingletonContext = new UpdateManager();
+                        }
+                    }
+                }
+                return SingletonContext;
+            }
+        }
+
+        #endregion Singleton
+
+        internal List<IUpdateSource> UpdateSources { get; set; }
+
+        internal UpdateManager()
+        {
+            UpdateSources = new List<IUpdateSource>();
+        }
+
+        public void AddSource(IUpdateSource source)
+        {
+            lock (UpdateSources)
+                UpdateSources.Add(source);
+        }
+
+        public async Task<IEnumerable<IUpdateSource>> CheckForUpdates()
+        {
+            IUpdateSource[] localList = null;
+            lock (UpdateSources)
+            {
+                localList = UpdateSources.ToArray();
+            }
+            var ret = new List<IUpdateSource>();
+            foreach (var source in localList)
+            {
+                try
+                {
+                    var avail = await source.IsUpdateAvailable();
+                    if (avail)
+                    {
+                        Log.Debug("[CheckForUpdates] Update available for " + source.Name);
+                    }
+                    else
+                    {
+                        Log.Debug("[CheckForUpdates] No update available for " + source.Name);
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Log.Warn("[CheckForUpdates] Error checking for update for " + source.Name, e);
+                } 
+            }
+            return ret;
+        }
+
+        public async Task<IUpdatePackage> DownloadUpdate(IUpdateSource source)
+        {
+            try
+            {
+                var tempDir = Path.GetTempPath();
+                var location = new DirectoryInfo(Path.Combine(tempDir,"Revise",source.Name,Guid.NewGuid().ToString()));
+                return await source.DownloadUpdate(location);
+            }
+            catch (Exception e)
+            {
+                Log.Warn("[DownloadUpdate] Error download update for " + source.Name,e);
+                return null;
+            }
+        }
+    }
+}
